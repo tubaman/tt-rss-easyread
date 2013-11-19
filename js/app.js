e473877497ecc98817169dfb83d0c3a1dde7d1d5
Ryan Nowakowski <tubaman@fattuba.com>
2013-11-18 18:33:33 -0600

    sorta working

--- js/app.js
+++ js/app.js
@@ -2,18 +2,82 @@ window.App = Ember.Application.create();
 
 App.Router.map(function() {
 	this.resource('categories', { path: '/' });
+	this.resource('feeds', { path: '/category/:category_id' });
+	this.resource('headlines', { path: '/feed/:feed_id' });
 });
 
 App.CategoriesRoute = Ember.Route.extend({
 	model: function() {
-		return App.TtRss.get('login')('admin', 'songahm').
-		then(function() {
-			return App.TtRss.get('categories').fetch();
+		App.Categories.fetch();
+		return App.Categories;
+	}
+});
+
+App.FeedsRoute = Ember.Route.extend({
+	model: function(params) {
+		App.Feeds.fetch(params.category_id);
+		return App.Feeds;
+	}
+});
+
+App.HeadlinesRoute = Ember.Route.extend({
+	model: function(params) {
+		App.Headlines.fetch(params.feed_id);
+		return App.Headlines;
+	}
+});
+
+/* models */
+App.Category = Ember.Object.extend({});
+App.Categories = Ember.ArrayProxy.create({
+	content: [],
+
+	fetch: function() {
+		var content = this.get('content');
+		var ttrss = new TtRss();
+		ttrss.getCategories().then(function(categories) {
+			content.clear();
+			categories.content.forEach(function(data) {
+				var category = App.Category.create();
+				category.setProperties(data);
+				content.pushObject(category);
+			});
 		});
 	}
 });
 
-App.ApplicationController = Ember.Controller.extend({
-	sid: $.cookie('ttrss_api_sid'),
+App.Feed = Ember.Object.extend({});
+App.Feeds = Ember.ArrayProxy.create({
+	content: [],
+
+	fetch: function(categoryId) {
+		var content = this.get('content');
+		var ttrss = new TtRss();
+		ttrss.getFeeds(categoryId).then(function(feeds) {
+			content.clear();
+			feeds.content.forEach(function(data) {
+				var feed = App.Feed.create();
+				feed.setProperties(data);
+				content.pushObject(feed);
+			});
+		});
+	}
 });
 
+App.Headline = Ember.Object.extend({});
+App.Headlines = Ember.ArrayProxy.create({
+	content: [],
+
+	fetch: function(feedId) {
+		var content = this.get('content');
+		var ttrss = new TtRss();
+		ttrss.getHeadlines(feedId).then(function(headlines) {
+			content.clear();
+			headlines.content.forEach(function(data) {
+				var headline = App.Headline.create();
+				headline.setProperties(data);
+				content.pushObject(headline);
+			});
+		});
+	}
+});
--- js/tt-rss.js
+++ js/tt-rss.js
@@ -8,15 +8,51 @@
  * headlines = ttrss.getHeadlines(feeds[0].id);
  * */
 
-App.TtRss = Ember.Object.extend({
-	sid: $.cookie('ttrss_api_sid'),
+function TtRss(url) {
 
-	_api: function(data) {
+	url = url || '/tt-rss/api/';
+
+	var self = this,
+		sid = null;
+		sid = Ember.$.cookie('ttrss_api_sid');
+
+	self.login = function(username, password) {
+		return api({op: 'login', user: username, password: password})
+			.done(function(data) {
+				sid = data.content.session_id;
+				return sid;
+			});
+	}
+
+	self.getCategories = function() {
+		return api({op: 'getCategories', enable_nested: 'false'})
+			.done(function(data) {
+				return data.content;
+			});
+	}
+
+	self.getFeeds = function(catId) {
+		return api({op: 'getFeeds', cat_id: catId, enable_nested: 'false'})
+			.done(function(data) {
+				return data.content;
+			});
+	}
+
+	self.getHeadlines = function(feedId) {
+		return api({op: 'getHeadlines', feed_id: feedId})
+			.done(function(data) {
+				return data.content;
+			});
+	}
+
+	/* private functions */
+	
+	function api(data) {
 		// If we're not logging in, include the session id
 		if (data.op != 'login') {
 			data.sid = sid;
 		}
-		return $.ajax({
+		return Ember.$.ajax({
 			type: 'POST',
 			url: url,
 			contentType: 'application/json',
@@ -25,100 +61,11 @@ App.TtRss = Ember.Object.extend({
 		})
 		.then(function(data, textStatus, jqXHR) {
 			if (data.content.error) {
-				return $.Deferred().reject(jqXHR, data, data.content.error).promise();
+				return Ember.$.Deferred().reject(jqXHR, data, data.content.error).promise();
 			} else {
 				return data;
 			}
 		});
-	},
+	}
 
-	login: function(username, password) {
-		return _api({op: 'login', user: username, password: password})
-			.done(function(data) {
-				sid = data.content.session_id;
-			});
-	},
-
-	Category: Ember.Object.create({}),
-
-	categories: Ember.ArrayProxy.create({
-		content: [],
-
-		fetch: function() {
-			var content = this.get('content');
-			_api({op: 'getCategories', enable_nested: 'false'})
-				.done(function(data) {
-					content.clear();
-					data.content.forEach(function(categoryData) {
-						var category = App.TtRss.Category.create();
-						category.setProperties(categoryData);
-						content.pushObject(category);
-					});
-				});
-		}
-
-	})
-});
-
-
-//function TtRss(url) {
-//
-//	url = url || '/tt-rss/api/';
-//
-//	var self = this,
-//		sid = null;
-//		sid = $.cookie('ttrss_api_sid');
-//
-//	self.login = function(username, password) {
-//		return api({op: 'login', user: username, password: password})
-//			.done(function(data) {
-//				sid = data.content.session_id;
-//				return sid;
-//			});
-//	}
-//
-//	self.getCategories = function() {
-//		return api({op: 'getCategories', enable_nested: 'false'})
-//			.done(function(data) {
-//				return data.content;
-//			});
-//	}
-//
-//	self.getFeeds = function(catId) {
-//		return api({op: 'getFeeds', cat_id: catId, enable_nested: 'false'})
-//			.done(function(data) {
-//				return data.content;
-//			});
-//	}
-//
-//	self.getHeadlines = function(feedId) {
-//		return api({op: 'getHeadlines', feed_id: feedId})
-//			.done(function(data) {
-//				return data.content;
-//			});
-//	}
-//
-//	/* private functions */
-//	
-//	function api(data) {
-//		// If we're not logging in, include the session id
-//		if (data.op != 'login') {
-//			data.sid = sid;
-//		}
-//		return $.ajax({
-//			type: 'POST',
-//			url: url,
-//			contentType: 'application/json',
-//			dataType: 'json',
-//			data: JSON.stringify(data)
-//		})
-//		.then(function(data, textStatus, jqXHR) {
-//			if (data.content.error) {
-//				return $.Deferred().reject(jqXHR, data, data.content.error).promise();
-//			} else {
-//				return data;
-//			}
-//		});
-//	}
-//
-//}
+}
